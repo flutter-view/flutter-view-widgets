31b0814c31116da727cedacb54793391c50277f6
Christian Vogel <chris.vogel@me.com>
2018-12-17 16:31:33 +0100

    only expose model in the exports

--- lib/flutter_view_tools.dart
+++ lib/flutter_view_tools.dart
@@ -2,8 +2,7 @@
 /// flutter-view tool http://flutter-view.io.
 library flutter_view_tools;
 
-export 'package:scoped_model/scoped_model.dart';
-
+export 'package:scoped_model/scoped_model.dart' show Model;
 export 'src/assign.dart';
 export 'src/lifecycle.dart';
 export 'src/reactive-widget.dart';
