ed0c9b196035f2db81d97a9bb5eac53b8fc086b2
Christian Vogel <chris.vogel@me.com>
2019-01-16 16:22:27 +0100

    formatted source code

--- lib/src/assign.dart
+++ lib/src/assign.dart
@@ -4,23 +4,23 @@ typedef Widget AssignedBuilder<T>(BuildContext context, T assignment);
 
 /// Flutter-view helper widget to allow assigning a value or expression
 /// to a new variable name.
-/// 
+///
 /// In flutter-view you would use it like this:
-/// 
+///
 /// ```pug
 /// assign(name='sum' :value='2*3')
 ///   | the sum is $sum
 /// ```
-/// 
+///
 /// Which flutter-view converts to this:
-/// 
+///
 /// ```dart
 /// return Assign<int>(
 ///   name: 'sum',
 ///   value: 2*3,
 ///   builder: (context, sum) => Text('the sum is $sum')
 /// );
-/// 
+///
 /// See the flutter-view documentation for more information.
 /// ```
 class Assign<T> extends StatelessWidget {
@@ -28,7 +28,7 @@ class Assign<T> extends StatelessWidget {
   final String name;
   final AssignedBuilder<T> builder;
 
-  Assign({@required this.name, @required this.value, @required this.builder });
+  Assign({@required this.name, @required this.value, @required this.builder});
 
   @override
   Widget build(BuildContext context) {
